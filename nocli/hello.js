bb7500cdaf3b006cc9e8903b2991e0caf4186088
cyupa <kerottomaati@icloud.com>
2022-07-26 03:59:44 +0900

    second

--- nocli/hello.js
+++ nocli/hello.js
@@ -1,6 +1,13 @@
-var app = new Vue({ 
+/*var app = new Vue({ 
     el: '#app',
     data: {
         message: 'Hello Vue!'
     }
 });
+*/
+new Vue({
+    el: "#app",
+    data: {
+        x1: 100
+    }
+})
